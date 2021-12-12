abaae9ccae17ba4f90b6bedbf15878da7f79987d
ahmed2fathy <spotlight.techno@gmail.com>
2021-12-12 21:56:49 +0200

    add main apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ## my_apps
+    'accounts',
+    'settings',
+    'blog',
+    'about',
+    'property',
 ]
 
 MIDDLEWARE = [
---
+++ property/to_do_.md
@@ -0,0 +1,17 @@
+ Property :
+    - properties
+    - booking
+    - rating
+
+ blog :
+    - posts
+    - categories 
+    - tags
+    - Comments
+
+About :
+    - About
+    - FAQ
+
+setting :
+   - Website info
